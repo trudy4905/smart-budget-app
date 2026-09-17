1d9e6d76c19c619d2ad92236c0989a35de00cbfa
trudy4905 <trudy4905@users.noreply.github.com>
2026-09-17 06:00:41 +0000

    deploy: e4f3477ab519531821f2ce1cd3812d4f72329481

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,8 +5,8 @@ const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"manifest.json": "8e66e1314dbabf0aa13062137d7bf013",
 "version.json": "322dfa94430cc69aa45ce83495f46466",
-"main.dart.js": "78e787e091f2dfbdc7c153564b3806a6",
-"flutter_bootstrap.js": "8140f71d20cdf871922639448d7f3ff3",
+"main.dart.js": "3f5077ece5c184fafda72c7464ca99dc",
+"flutter_bootstrap.js": "51e375ab27848538ff7609d699ccda8c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
