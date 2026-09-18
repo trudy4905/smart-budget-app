9027eb8a9ab9d2c163086bb360bf3c323afb4208
trudy4905 <trudy4905@users.noreply.github.com>
2026-09-18 09:09:18 +0000

    deploy: 8ee25d7b4e90f501f75630c6d34180c3dcc138c6

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "394131136"
+    serviceWorkerVersion: "2563033228"
   }
 });
--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -5,8 +5,8 @@ const CACHE_NAME = 'flutter-app-cache';
 
 const RESOURCES = {"manifest.json": "8e66e1314dbabf0aa13062137d7bf013",
 "version.json": "322dfa94430cc69aa45ce83495f46466",
-"main.dart.js": "840e5fdd8ba31f490bfc882d528c39a7",
-"flutter_bootstrap.js": "a668db2b4620303013d37eeeb700ecc1",
+"main.dart.js": "94c2131e5a23e57ca46d6b5354987e17",
+"flutter_bootstrap.js": "466beb89203ec6fc40e069bbc76e8a6c",
 "icons/Icon-maskable-512.png": "301a7604d45b3e739efc881eb04896ea",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-maskable-192.png": "c457ef57daa1d16f64b27b786ec2ea3c",
