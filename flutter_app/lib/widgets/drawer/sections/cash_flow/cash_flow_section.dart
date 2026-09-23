23f7bd885ee81442a6ce3715a7070441b9f223fd
trudy4905 <trudy4905@gmail.com>
2026-09-23 16:07:24 +0900

    Move expected_asset_card to cash_flow section

--- flutter_app/lib/widgets/drawer/sections/cash_flow/cash_flow_section.dart
+++ flutter_app/lib/widgets/drawer/sections/cash_flow/cash_flow_section.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import '../../../../providers/app_state.dart';
 import 'cash_flow_widgets.dart';
-import '../assets/expected_asset_card.dart';
+import 'expected_asset_card.dart';
 import 'recurring/recurring_expense_section.dart';
 import 'recurring/recurring_income_section.dart';
 import 'drawer_side_panel.dart';
